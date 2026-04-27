9601605e955439d289c4b93d094712e60e78d0c5
Marcos Villeda <marcosvilleda@Marcoss-MacBook-Air.local>
2026-04-27 19:53:20 +0100

    Fix blank ticket downloads by reverting unstable canvas mode

--- tickets.js
+++ tickets.js
@@ -170,7 +170,6 @@ async function downloadCard(cardElement, filename) {
         backgroundColor: '#fdffff',
         scale: 2,
         useCORS: true,
-        foreignObjectRendering: true,
         ignoreElements: (element) => element.classList && element.classList.contains('ticket-actions')
     }).then(canvas => {
         const link = document.createElement('a');
